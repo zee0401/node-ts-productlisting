1742cc3a4e2297664dfe62f2080150359653e4ac
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-21 06:54:04 +0530

    .env

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,6 +1,9 @@
 import { DataSource } from "typeorm";
 import { Product } from "../entity/Product";
 import { ProductImage } from "../entity/Product-Image";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
