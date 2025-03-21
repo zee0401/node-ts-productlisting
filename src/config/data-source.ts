aa0bee39ef14168c36f096ae71b19f0709cebfd4
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-21 06:28:04 +0530

    datasource

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -4,14 +4,9 @@ import { ProductImage } from "../entity/Product-Image";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    // url: process.env.DATABASE_URL,
-    database: "product",
-    username: "postgres",
-    password: "",
-    host: "localhost",
-    port: 5432,
+    url: process.env.DATABASE_URL,
     synchronize: true,
-    // logging: true,
+    logging: true,
     entities: [Product, ProductImage],
     subscribers: [],
     migrations: [],
